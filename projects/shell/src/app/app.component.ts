33dd73e46dca482b1ce112da2652f925cea923c8
Loiane <loianeg@gmail.com>
2025-01-02 21:26:19 -0500

    add federation configuration and integrate header component in shell application

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-shell-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-shell-header></app-shell-header>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
-  title = 'shell';
 }

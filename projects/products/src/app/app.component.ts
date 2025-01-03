cfe5b78fd2e8960e06704dc201b6325debb3622d
Loiane <loianeg@gmail.com>
2025-01-02 22:12:49 -0500

    replace RouterOutlet with MatCardModule in app component

--- projects/products/src/app/app.component.ts
+++ projects/products/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-products-root',
-  imports: [RouterOutlet],
+  imports: [MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'products';
 }

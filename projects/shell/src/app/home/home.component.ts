7e39c09659bd21c1a9eddc176fb170603d83fc06
Loiane <loianeg@gmail.com>
2025-01-02 21:30:40 -0500

    add HomeComponent with routing and basic layout

---
+++ projects/shell/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-shell-home',
+  imports: [MatCardModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}

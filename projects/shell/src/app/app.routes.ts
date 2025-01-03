1efba1761ee89c4d23a442c704610a84cb876466
Loiane <loianeg@gmail.com>
2025-01-02 22:13:26 -0500

    add routing for products and update home component path matching

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: 'products',
+    loadComponent: () =>
+        loadRemoteModule('products', './Component').then((m) => m.AppComponent),
+  },
   {
       path: '**',
       component: HomeComponent,
